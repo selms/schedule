53c6330d4dc6a35f1d16615da8ded7ae3d9968b2
Selma Batista <selmsb@gmail.com>
2018-07-03 15:07:12 -0600

    built schedulecourse component and placed on grid

---
+++ src/components/schedule/scheduleCourse.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class ScheduleCourse extends Component {
+    render() {
+        return (
+        <div className="schedule-course">
+                <label>{this.props.title}</label>
+        
+        </div>
+        )
+    }
+}
+
+export default ScheduleCourse;
