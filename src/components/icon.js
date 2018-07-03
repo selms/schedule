9573f0db334a23b19acdacc55777e0bb62077f19
Selma Batista <selmsb@gmail.com>
2018-07-03 13:32:36 -0600

    intergrated font awesome and built a custom icon component

---
+++ src/components/icon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Icon = (icon, className) => {
+    return (
+      <i className={`${icon} ${className}`}></i>
+      
+      
+    )
+}
+
+export default Icon;
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class LibraryCourse extends Component {
     render() {
         return (
         <div className="library-course">
             <label className="library-course__title">Problem Solving</label>
-             {/* icon component */}
+             { Icon('fas fa-check', 'library-course__icon') }
              {/* arrow component */}
              {/* action button component */}
+             { Icon('fas fa-plus-circle', 'library-course__action')}
             <div className="library-course__description">
                 <label>Course Description</label>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id tincidunt elit. Proin at tristique ligula. Sed quis orci ac massa ultrices tempor. Nullam tortor erat, dictum sit amet ex sit amet, sollicitudin semper nisi.</p>
